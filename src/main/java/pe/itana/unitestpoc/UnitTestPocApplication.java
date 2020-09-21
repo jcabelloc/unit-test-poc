9275ee82923c14ca1e9e172d6eaa6cce090399e9
jcabelloc <jcabelloc@itana.pe>
2020-09-20 22:00:45 -0500

    Commit inicial

---
+++ src/main/java/pe/itana/unitestpoc/UnitTestPocApplication.java
@@ -0,0 +1,13 @@
+package pe.itana.unitestpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/itana/unitestpoc/UnitTestPocApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.itana.unitestpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
