32e1b6381629ebf390bc8525528c7073e8d5c18e
jcabelloc <jcabelloc@itana.pe>
2020-10-11 09:38:34 -0500

    Formateo de codigo

--- src/main/java/pe/itana/unitestpoc/utils/ZytrustException.java
+++ src/main/java/pe/itana/unitestpoc/utils/ZytrustException.java
@@ -1,15 +1,18 @@
 package pe.itana.unitestpoc.utils;
 
-public class ZytrustException extends RuntimeException{
+public class ZytrustException extends RuntimeException {
   
-  private String errorCode;
+  private static final long serialVersionUID = 1L;
+
+  private final String errorCode;
   
-  public ZytrustException() {
-    
-  }
-  
+
   public ZytrustException(String errorCode) {
     this.errorCode = errorCode;
+  }
+
+  public String getErrorCode() {
+    return errorCode;
   }
   
 }
